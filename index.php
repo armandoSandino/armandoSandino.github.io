f651f46e0f0d61f45981a1b82bdc5acd7b583180
Juan Sandino <juan.sandino421@gmail.com>
2019-02-27 17:03:53 -0600

    blog en construccion

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+require_once("php/cabecera.php");
+?>
+    <div class="container">
+        <div class="section">
+            <div class="col span_2_of_3">
+                <div class="blog-post">
+                    <h1 class="blog-title">Flutter</h1>
+                    <h2 class="date">Publicado 28 Feb 2015</h2>
+                    <img src="img/img1.jpg" alt="">
+                    <p class="blog-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, similique rerum dicta vel accusamus repellat laboriosam modi eaque aperiam nostrum nihil voluptatibus voluptatum mollitia eos aliquid harum, unde nobis ad consectetur earum possimus, delectus nemo. Temporibus laudantium ducimus quis, mollitia placeat, doloremque laboriosam atque provident et, non explicabo distinctio esse quo ipsam ab natus dignissimos? Consectetur animi sequi rerum. Optio voluptates eos, dolorem at adipisci blanditiis hic commodi sed nobis, id nisi iusto. Placeat sit, iure veniam atque repudiandae obcaecati.</p>
+                </div>
+                
+                <div class="blog-post">
+                    <h1 class="blog-title">REST API</h1>
+                    <h2 class="date">Publicado 28 Feb 2015</h2>
+                    <p class="blog-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, similique rerum dicta vel accusamus repellat laboriosam modi eaque aperiam nostrum nihil voluptatibus voluptatum mollitia eos aliquid harum, unde nobis ad consectetur earum possimus, delectus nemo. Temporibus laudantium ducimus quis, mollitia placeat, doloremque laboriosam atque provident et, non explicabo distinctio esse quo ipsam ab natus dignissimos? Consectetur animi sequi rerum. Optio voluptates eos, dolorem at adipisci blanditiis hic commodi sed nobis, id nisi iusto. Placeat sit, iure veniam atque repudiandae obcaecati.</p>
+                </div>
+                
+                <div class="blog-post">
+                    <h1 class="blog-title">FCM</h1>
+                    <h2 class="date">Publicado 28 Feb 2015</h2>
+                    <img src="img/img2.jpg" alt="">
+                    <p class="blog-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, similique rerum dicta vel accusamus repellat laboriosam modi eaque aperiam nostrum nihil voluptatibus voluptatum mollitia eos aliquid harum, unde nobis ad consectetur earum possimus, delectus nemo. Temporibus laudantium ducimus quis, mollitia placeat, doloremque laboriosam atque provident et, non explicabo distinctio esse quo ipsam ab natus dignissimos? Consectetur animi sequi rerum. Optio voluptates eos, dolorem at adipisci blanditiis hic commodi sed nobis, id nisi iusto. Placeat sit, iure veniam atque repudiandae obcaecati.</p>
+                </div>
+
+                <div class="blog-post">
+                    <h1 class="blog-title">REST API</h1>
+                    <h2 class="date">Publicado 28 Feb 2015</h2>
+                    <p class="blog-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, similique rerum dicta vel accusamus repellat laboriosam modi eaque aperiam nostrum nihil voluptatibus voluptatum mollitia eos aliquid harum, unde nobis ad consectetur earum possimus, delectus nemo. Temporibus laudantium ducimus quis, mollitia placeat, doloremque laboriosam atque provident et, non explicabo distinctio esse quo ipsam ab natus dignissimos? Consectetur animi sequi rerum. Optio voluptates eos, dolorem at adipisci blanditiis hic commodi sed nobis, id nisi iusto. Placeat sit, iure veniam atque repudiandae obcaecati.</p>
+                </div>
+                
+                <div class="blog-post">
+                    <h1 class="blog-title">FCM</h1>
+                    <h2 class="date">Publicado 28 Feb 2015</h2>
+                    <img src="img/img2.jpg" alt="">
+                    <p class="blog-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, similique rerum dicta vel accusamus repellat laboriosam modi eaque aperiam nostrum nihil voluptatibus voluptatum mollitia eos aliquid harum, unde nobis ad consectetur earum possimus, delectus nemo. Temporibus laudantium ducimus quis, mollitia placeat, doloremque laboriosam atque provident et, non explicabo distinctio esse quo ipsam ab natus dignissimos? Consectetur animi sequi rerum. Optio voluptates eos, dolorem at adipisci blanditiis hic commodi sed nobis, id nisi iusto. Placeat sit, iure veniam atque repudiandae obcaecati.</p>
+                </div>
+                
+            </div>
+
+            <aside class="col span1_of_3">
+                <div class="side-post">
+                    <img src="img/img3.png" alt="">
+                    <p class="side-content"> Aqui puedes encontrar todo recurso informatico tanto para app de escritorio, moviles y webs.
+                    </p>
+                </div>
+
+                <div class="side-post">
+                    <p class="side-content">Te interezan los temas relacionados al mundo tecnologico, Suscribete, solo es un minuto.</p>
+                    <a href="#" class="button">Suscribete!</a>
+                </div>
+            </aside>
+
+        </div>
+    </div>
+
+<?php require_once("php/footer.php"); ?>
+
+
+
+
+
+
+
+
